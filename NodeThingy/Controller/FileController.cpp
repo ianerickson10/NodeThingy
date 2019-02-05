ff2cbdd1cac69f72a1d3a0bfddb6e3e051b639cd
Ian Erickson <19ierickson@gmail.com>
2019-02-05 13:07:16 -0700

    added file controllers

---
+++ NodeThingy/Controller/FileController.cpp
@@ -0,0 +1,9 @@
+//
+//  FileController.cpp
+//  NodeThingy
+//
+//  Created by Erickson, Ian on 2/5/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "FileController.hpp"
---
+++ NodeThingy/Controller/FileController.hpp
@@ -0,0 +1,20 @@
+//
+//  FileController.hpp
+//  NodeThingy
+//
+//  Created by Erickson, Ian on 2/5/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef FileController_hpp
+#define FileController_hpp
+
+#include "../Resources/CrimeData.hpp"
+#include "../Resources/Music.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+#endif /* FileController_hpp */
