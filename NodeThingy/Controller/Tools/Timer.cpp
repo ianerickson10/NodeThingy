b6fa2062e31a07e87caf8745605eedf5e4db040b
Ian Erickson <19ierickson@gmail.com>
2019-02-05 12:30:02 -0700

    added timer classes

---
+++ NodeThingy/Controller/Tools/Timer.cpp
@@ -0,0 +1,43 @@
+//
+//  Timer.cpp
+//  NodeThingy
+//
+//  Created by Erickson, Ian on 2/5/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Timer.hpp"
+
+using namespace std;
+
+Timer :: Timer()
+{
+    executionTime = 0;
+}
+
+void Timer :: resetTimer()
+{
+    executionTime = 0;
+}
+
+void Timer :: startTimer()
+{
+    exacutionTime = clock();
+}
+
+void Timer :: stopTimer()
+{
+    assert(executionTime != 0);
+    executionTime = clock() - executionTime;
+}
+
+void Timer :: displayInfomation()
+{
+    cout << "The ececution time is: " << executionTime << endl;
+    cout << "In normal time it is " << double (executionTime)/CLOCKS_PER_SEC << " seconds" << endl;
+}
+
+long Timer :: getTimeInMicroseconds()
+{
+    return executionTime;
+}
