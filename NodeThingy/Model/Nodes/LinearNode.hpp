6d022d5910ecc0bb4329675f6ebb9577a802d905
Ian Erickson <19ierickson@gmail.com>
2019-02-07 13:34:00 -0700

    added a few methods to LinearNode

--- NodeThingy/Model/Nodes/LinearNode.hpp
+++ NodeThingy/Model/Nodes/LinearNode.hpp
@@ -14,25 +14,34 @@
 template <class Type>
 class LinearNode : public Node<Type>
 {
-private:
+protected:
     LinearNode<Type> * next;
 public:
     LinearNode();
     LinearNode(Type data);
     LinearNode(Type data, LinearNode<Type> * next);
-    LinearNode<Type> * getNext();
-    void setNext(LinearNode<Type> * next);
+    LinearNode<Type> * getNextNode();
+    void setNextNode(LinearNode<Type> * next);
 };
 
 //Implementaion of templates
 template <class Type>
 LinearNode<Type> :: LinearNode() : Node<Type>()
-{}
+{
+    this->next = nullptr;
+}
 
 template <class Type>
 LinearNode<Type> :: LinearNode(Type data) : Node<Type>(data)
 {
     this->next = nullptr;
 }
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data, LinearNode<Type> * next) : Node<Type>(data)
+{
+    this->next = next;
+}
+
 
 #endif /* LinearNode_h */
