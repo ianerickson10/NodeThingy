cc5277ff0854608dae864b8fda4da85a57e834a8
Ian Erickson <19ierickson@gmail.com>
2019-02-11 13:39:57 -0700

    finished constructor of Queue

---
+++ NodeThingy/Queue.hpp
@@ -0,0 +1,35 @@
+//
+//  Queue.h
+//  NodeThingy
+//
+//  Created by Erickson, Ian on 2/11/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Queue_hpp
+#define Queue_hpp
+
+#include "LinkedList.hpp"
+
+#include <iostream>
+using namespace std;
+
+template <class Type>
+class Queue : public LikedList<Type>
+{
+public:
+    Queue();
+    ~Queue();
+    
+    void enqueue(Type data);
+    Type dequeue();
+    Type peek();
+    void clear();
+    
+    void add(Type data);
+    void addAtIndex(int index, Type data);
+    Type getFromIndex(int index);
+    Type remove(int index);
+}
+
+#endif /* Queue_h */
