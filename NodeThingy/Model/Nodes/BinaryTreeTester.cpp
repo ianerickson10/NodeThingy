bd8fa26112bbcc0c3503e2e0ca2463fe93f027c2
Ian Erickson <19ierickson@gmail.com>
2019-04-18 12:47:59 -0600

    added TreeTester

---
+++ NodeThingy/Model/Nodes/BinaryTreeTester.cpp
@@ -0,0 +1,9 @@
+//
+//  BinaryTreeTester.cpp
+//  NodeThingy
+//
+//  Created by Erickson, Ian on 4/16/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "BinaryTreeTester.hpp"
---
+++ NodeThingy/Model/Nodes/BinaryTreeTester.hpp
@@ -0,0 +1,18 @@
+//
+//  BinaryTreeTester.hpp
+//  NodeThingy
+//
+//  Created by Erickson, Ian on 4/16/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_hpp
+#define BinaryTreeTester_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+#include "../Model/Nodes/Tree.hpp"
+#include "../Model/"
+
+#endif /* BinaryTreeTester_hpp */
